cf72e99d8a51f2bee09e995243374ba9693b6bbc
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2012-06-13 22:58:57 +0300

    rough working version of the webchat: implemented the SSEs, the AJAX requests and the messaging system in the backend

--- src/index.py
+++ src/index.py
@@ -1,15 +1,28 @@
 #! /usr/bin/env python2.7
 
-from flask import Flask, render_template, redirect, url_for, session, Response
+from flask import Flask, render_template, redirect, url_for, session, Response, request
 from forms import ConnectForm, ChatForm
 from simplekv.fs import FilesystemStore
 from flaskext.kvsession import KVSessionExtension
+from redis import Redis
 
 app = Flask(__name__)
 app.secret_key = 'populate this string yourself!'
 
 store = FilesystemStore('data')
 sess_ext = KVSessionExtension(store, app)
+
+r = Redis()
+
+def event_stream():
+    #TODO: check for errors
+    pubsub = r.pubsub()
+    pubsub.subscribe('webchat')
+
+    for event in pubsub.listen():
+        print event
+        yield 'data: {0}\n\n'.format(event)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -44,7 +57,8 @@ def chat():
         return redirect(url_for('index'))
 
     if form.send.data and form.text.validate(form):
-        return send_event()
+        pass
+        #return send_event()
         #this part causes the browser to download the data
         #just send the FORM via AJAX and handle the event sending in something
         #like receive_message()
@@ -52,15 +66,23 @@ def chat():
     return render_template('chat.html', nick=session['nick'], form=form)
 
 
+@app.route('/_receive_message', methods=['POST'])
+def receive_message():
+    print request.form['message']
+    #if the request cannot be fulfilled return 500 with the explanation
+    r.publish('webchat', request.form['message'])
+
+    return 'ok'
+
 @app.route('/_send_event')
 def send_event():
-    return Response('data: TODO JSON the event data\n\n', mimetype='text/event-stream',
+    return Response(event_stream(), mimetype='text/event-stream',
             headers={'Cache-Control': 'no-cache'})
 
 
 if __name__ == '__main__':
     sess_ext.cleanup_sessions()
-    app.run(debug=True)
+    app.run(debug=True, threaded=True, port=5001)
 
     #TODO: handle the user logout, via PING-PONG maybe?
     #TODO: check the nick uniqueness
--- src/static/chat.js
+++ src/static/chat.js
@@ -1,3 +1,13 @@
+function get_status(data){
+/*add here a callback that will get the status code of the page and in case of an error it will display an error to the user*/
+}
+
+function send_message(e){
+    e.preventDefault();
+    $.post('/_receive_message', {'message': $('input:text').val()});
+}
+
+
 function load_chat(){
     console.log('loaded');
     var stream = new EventSource('/_send_event');
@@ -6,3 +16,4 @@ function load_chat(){
 }
 
 load_chat();
+$('[name="send"]').click(send_message);
