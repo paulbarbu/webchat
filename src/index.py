8c9785abfd47603247bac2e5b709d41a25b723c7
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2012-06-14 21:30:57 +0300

    cleanup and providing error events

--- src/index.py
+++ src/index.py
@@ -1,10 +1,15 @@
 #! /usr/bin/env python2.7
 
-from flask import Flask, render_template, redirect, url_for, session, Response, request
+from flask import (
+    Flask, render_template, redirect, url_for, session, Response, request
+)
 from forms import ConnectForm, ChatForm
 from simplekv.fs import FilesystemStore
 from flaskext.kvsession import KVSessionExtension
-from redis import Redis
+import redis
+import logging
+
+import err
 
 app = Flask(__name__)
 app.secret_key = 'populate this string yourself!'
@@ -12,17 +17,7 @@ app.secret_key = 'populate this string yourself!'
 store = FilesystemStore('data')
 sess_ext = KVSessionExtension(store, app)
 
-r = Redis()
-
-def event_stream():
-    #TODO: check for errors
-    pubsub = r.pubsub()
-    pubsub.subscribe('webchat')
-
-    for event in pubsub.listen():
-        print event
-        yield 'data: {0}\n\n'.format(event)
-
+r = redis.Redis()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -34,8 +29,9 @@ def index():
     form = ConnectForm()
 
     if form.validate_on_submit():
+        #TODO: check the uniqness of the name
         session['nick'] = form.nick.data
-        session.regenerate()
+        session.regenerate() # anti session-fixation attack
 
         return redirect(url_for('chat'))
 
@@ -47,43 +43,70 @@ def chat():
     sess_ext.cleanup_sessions()
     form = ChatForm()
 
-    if 'nick' not in session: #user has no nickname
+    if 'nick' not in session: #user has no nickname (he's not logged in)
         return redirect(url_for('index'))
 
     if form.quit.data:
         session.destroy()
         sess_ext.cleanup_sessions()
+        #TODO: update the user list on exit, check if the browser closes
+        #without the quit btn being pressed
 
         return redirect(url_for('index'))
-
-    if form.send.data and form.text.validate(form):
-        pass
-        #return send_event()
-        #this part causes the browser to download the data
-        #just send the FORM via AJAX and handle the event sending in something
-        #like receive_message()
 
     return render_template('chat.html', nick=session['nick'], form=form)
 
 
-@app.route('/_receive_message', methods=['POST'])
-def receive_message():
-    print request.form['message']
-    #if the request cannot be fulfilled return 500 with the explanation
-    r.publish('webchat', request.form['message'])
+@app.route('/_publish_message', methods=['POST'])
+def publish_message():
+    try:
+        #TODO: sanitize data and pack it into JSON before publishing
+        #security: Redis, XSS
+        #validity: non-blank msg and len(msg) >= 1
+        r.publish('webchat', request.form['message'])
+    except redis.ConnectionError as e:
+        logging.critical(e)
+        return Response(err.ConnectionError, 500)
+    except redis.RedisError as e:
+        logging.critical(e)
+        return Response(err.UnexpectedBackendError, 500)
+    except Exception as e:
+        logging.critical(e)
+        return Response(err.UnexpectedError, 500)
 
-    return 'ok'
+    return 'Received!'
 
-@app.route('/_send_event')
-def send_event():
-    return Response(event_stream(), mimetype='text/event-stream',
-            headers={'Cache-Control': 'no-cache'})
+
+@app.route('/_sse_stream')
+def sse_stream():
+    return Response(get_event(), mimetype='text/event-stream',
+        headers={'Cache-Control': 'no-cache'})
+
+
+def get_event():
+    try:
+        pubsub = r.pubsub()
+        pubsub.subscribe('webchat')
+    except redis.RedisError as e:
+        logging.critical(e)
+        yield 'event: error\ndata: {0}\n\n'.format(err.UnexpectedBackendError)
+    except Exception as e:
+        logging.critical(e)
+        yield 'event: error\ndata: {0}\n\n'.format(err.UnexpectedError)
+    else:
+        for event in pubsub.listen():
+            yield 'data: {0}\n\n'.format(event['data'])
 
 
 if __name__ == '__main__':
+    logging.basicConfig(filename='logs.log', level=logging.DEBUG,
+            format='%(levelname)s: %(asctime)s - %(message)s',
+            datefmt='%d-%m-%Y %H:%M:%S')
+
     sess_ext.cleanup_sessions()
     app.run(debug=True, threaded=True, port=5001)
 
     #TODO: handle the user logout, via PING-PONG maybe?
-    #TODO: check the nick uniqueness
-    #TODO: set a lengthier reconnect time for the EventSource
+    #TODO: show a user list
+    #TODO: add class for the yielded events
+    #TODO: usage limiter (per user)!
--- src/static/chat.js
+++ src/static/chat.js
@@ -1,19 +1,35 @@
-function get_status(data){
-/*add here a callback that will get the status code of the page and in case of an error it will display an error to the user*/
+function display_publish_error(){
+    //TODO: display an error to the user (into the chat)
+    console.log('xhr fail')
 }
 
-function send_message(e){
+function publish_message(e){
     e.preventDefault();
-    $.post('/_receive_message', {'message': $('input:text').val()});
+    $.post('/_publish_message', {'message': $('input:text').val()})
+        .fail(display_publish_error);
 }
 
+function handle_event_error(e){
+    this.close(); //`this` refers to `stream` in this context
+    console.log('err:', e);
+    //TODO: display some error to the user here (some kind fo pop up) with
+    //e.data as the err msg, also show a reconnect btn with a timer for
+    //auto-reconnect
+}
+
+function handle_message_event(e){
+    //TODO: display the message unpacked from JSON into the chat
+    console.log(e);
+}
 
 function load_chat(){
-    console.log('loaded');
-    var stream = new EventSource('/_send_event');
+    var stream = new EventSource('/_sse_stream');
 
-    stream.addEventListener('message', function(e){console.log(e)}, false);
+    stream.onmessage = handle_message_event;
+    stream.onerror = handle_event_error;
 }
 
 load_chat();
-$('[name="send"]').click(send_message);
+$('[name="send"]').click(publish_message);
+
+//TODO: add class for handle_event_*
