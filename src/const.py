6034457f292dd4316d27e139f21f6f54ee5d3ec9
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2012-06-15 22:00:43 +0300

    added the needed constants for the new security measures

--- src/const.py
+++ src/const.py
@@ -3,4 +3,5 @@ ConnectionError = 'Connection error!'
 UnexpectedError = 'Unexpected error!'
 
 Received = 'Received'
-EmptyMessage = 'empty message was supplied, no action taken!'
+EmptyMessage = 'Empty message was supplied, no action taken!'
+NotAuthentifiedError = 'You have to authentify in order to use the chat!'
