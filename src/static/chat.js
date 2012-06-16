718df9a9489a6c85d43c17e6d81aacf86a931400
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2012-06-16 14:07:40 +0300

    the handlers are now namespaced

--- src/static/chat.js
+++ src/static/chat.js
@@ -1,68 +1,65 @@
-function display_publish_error(){
-    var lineDiv = document.createElement('div');
+Handler = {
+    event_message: function handle_event_message(e){
+        var lineDiv = document.createElement('div');
 
-    lineDiv.className = 'line error';
+        var timeSpan = document.createElement('span');
+        var nickSpan = document.createElement('span');
+        var msgSpan = document.createElement('span');
 
-    lineDiv.innerHTML = 'Error sending your message!';
+        var data = JSON.parse(e.data);
 
-    $('#chat').append(lineDiv);
+        timeSpan.innerHTML = data['time'] + ' ';
+        timeSpan.className = 'time';
+
+        nickSpan.innerHTML = data['nick'] + ': ';
+        nickSpan.className = 'nick';
+
+        msgSpan.innerHTML = data['message'] + ' ';
+        msgSpan.className = 'msg';
+
+
+        lineDiv.className = 'line';
+        lineDiv.appendChild(timeSpan);
+        lineDiv.appendChild(nickSpan);
+        lineDiv.appendChild(msgSpan);
+
+        $('#chat').append(lineDiv);
+
+        //TODO: scroll the div to the bottom of the page when the content is larger
+        //than the div
+    },
+
+    publish_error: function handle_publish_error(){
+        var lineDiv = document.createElement('div');
+
+        lineDiv.className = 'line error';
+        lineDiv.innerHTML = 'Error sending your message!';
+
+        $('#chat').append(lineDiv);
+    },
+
+    event_error: function handle_event_error(e){
+        this.close(); //here, `this` refers to `stream`
+        console.log('err:', e);
+        //TODO: display some error to the user here (some kind of pop up) with
+        //e.data as the err msg, also show a reconnect btn with a timer for
+        //auto-reconnect
+    }
 }
 
 function publish_message(e){
     e.preventDefault();
     $.post('/_publish_message', {'message': $('input:text').val()})
-        .fail(display_publish_error);
+        .fail(Handler.publish_error);
     $('input:text').val('');
-}
-
-function handle_event_error(e){
-    this.close(); //here, `this` refers to `stream`
-    console.log('err:', e);
-    //TODO: display some error to the user here (some kind of pop up) with
-    //e.data as the err msg, also show a reconnect btn with a timer for
-    //auto-reconnect
-}
-
-function handle_message_event(e){
-    var lineDiv = document.createElement('div');
-
-    var timeSpan = document.createElement('span');
-    var nickSpan = document.createElement('span');
-    var msgSpan = document.createElement('span');
-
-    var data = JSON.parse(e.data);
-
-    lineDiv.className = 'line';
-
-    timeSpan.innerHTML = data['time'] + ' ';
-    timeSpan.className = 'time';
-
-    nickSpan.innerHTML = data['nick'] + ': ';
-    nickSpan.className = 'nick';
-
-    msgSpan.innerHTML = data['message'] + ' ';
-    msgSpan.className = 'msg';
-
-
-    lineDiv.appendChild(timeSpan);
-    lineDiv.appendChild(nickSpan);
-    lineDiv.appendChild(msgSpan);
-
-    $('#chat').append(lineDiv);
-
-    //TODO: scroll the div to the bottom of the page when the content is larger
-    //than the div
 }
 
 function load_chat(){
     var stream = new EventSource('/_sse_stream');
 
-    stream.onmessage = handle_message_event;
-    //stream.addEventListener('message', handle_message_event);
-    stream.onerror = handle_event_error;
+    stream.onmessage = Handler.event_message;
+    stream.onerror = Handler.event_error;
 }
 
 load_chat();
 $('[name="send"]').click(publish_message);
-
-//TODO: add class for handle_event_* as a Strategy pattern?
