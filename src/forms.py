6cdce07429e66d624c99f7aa14a37e9009fc233c
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2012-08-11 14:04:19 +0300

    refined the shown messages for issue #28 and set a margin to the body

--- src/forms.py
+++ src/forms.py
@@ -1,19 +1,19 @@
-from flaskext.wtf import Form, TextField, validators, SubmitField, HiddenField
+from flaskext.wtf import Form, TextField, validators, SubmitField, HiddenField, TextAreaField
 
 room_allowed_chars = 'Room names may be composed only of alphanumeric characters and underscores!'
 
 #TODO: add reCaptcha field
 class ConnectForm(Form):
     nick = TextField('Nick', [validators.Length(min=3,
-        message='Nick must be at least 3 characters long!'), validators.Regexp('\w',
-        message='Nick must be composed only of alphanumeric characters and underscores!')])
+        message='Your nickname must be at least 3 characters long!'), validators.Regexp('\w',
+        message='Your nickname must be composed only of alphanumeric characters and underscores!')])
     rooms = TextField('Rooms (separated by spaces)', [validators.regexp('\w',
         message=room_allowed_chars),
         validators.Optional()])
     submit = SubmitField('Connect!')
 
 class ChatForm(Form):
-    text = TextField()
+    text = TextAreaField()
     rooms = HiddenField('rooms')
     join_rooms = TextField('Join rooms (separated by spaces)', [validators.regexp('\w',
         message=room_allowed_chars),
