3c5dc12cde58a699f1b1153006a501712f3037a2
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2012-09-06 20:54:58 +0300

    Started working at the Chat page

---
+++ webchat/Controllers/ChatController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace webchat.Controllers
+{
+    public class ChatController : Controller
+    {
+        //
+        // GET: /Chat/
+
+        public ActionResult Index()
+        {
+            if(null == Session["nick"]) {
+                return RedirectToAction("Index", "Index");
+            }
+
+            return View();
+        }
+
+    }
+}
