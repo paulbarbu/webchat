a42e2b6ed9c5aceefd9ca4e1ce50b6c4e7b726d9
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2012-09-07 12:14:36 +0300

    Added ErrorSummaryHelper to display errors

---
+++ webchat/Helpers/ErrorSummary.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace webchat.Helpers {
+    public static class ErrorSummaryHelper {
+        public static IHtmlString ErrorSummary(this HtmlHelper helper, string id, List<string> errors) {
+            return ErrorSummary(helper, id, errors, null, null);
+        }
+
+        public static IHtmlString ErrorSummary(this HtmlHelper helper, string id, List<string> errors,
+            object outerHtmlAttributes, object innertHtmlAttributes) {
+            
+            TagBuilder containerBuilder = new TagBuilder("div");
+
+            containerBuilder.GenerateId(id);
+            containerBuilder.MergeAttributes(new RouteValueDictionary(outerHtmlAttributes));
+
+            foreach(var error in errors){
+                TagBuilder errorBuilder = new TagBuilder("div");
+
+                errorBuilder.MergeAttributes(new RouteValueDictionary(innertHtmlAttributes));
+                errorBuilder.SetInnerText(error);
+
+                containerBuilder.InnerHtml += errorBuilder.ToString();
+            }
+
+            return new HtmlString(containerBuilder.ToString());
+        }
+    }
+}
