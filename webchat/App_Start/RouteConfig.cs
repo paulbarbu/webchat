5a8b7a619cf4f40a147aa0d4367d839375f31190
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2012-09-07 21:31:21 +0300

    The default route calls Index.Index() now

--- webchat/App_Start/RouteConfig.cs
+++ webchat/App_Start/RouteConfig.cs
@@ -13,7 +13,7 @@ namespace webchat {
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Index", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
